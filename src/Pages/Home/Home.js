89e2c6fced032220ad74f692c29227af267914dc
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-12-28 00:45:15 +0600

    modified home and added auth context

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import {BsImages} from 'react-icons/bs';
 
 const Home = () => {
   return (
-    <div>
-      
+    <div className='grid grid-cols-4 mt-5'>
+      <div>
+        <h2>section</h2>
+      </div>
+      <div className='p-5 col-span-2 bg-[#2b2b2b] rounded-lg'>
+        <div className='flex'>
+          <input type="text" placeholder="What's on your mind? " className="input input-bordered border-2 border-transparent focus:outline-none focus:border-2 focus:border-blue-500 bg-zinc-900 w-full rounded-full" />
+          <div className="tooltip tooltip-bottom" data-tip="Upload Image"><button className='btn rounded-full ml-2'><BsImages className='text-red-500'/></button></div>
+        </div>
+      </div>
+      <div>
+        <h2>section</h2>
+      </div>
     </div>
   );
 };
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -6,26 +6,26 @@ import './Header.css';
 
 const Header = () => {
   return (
-    <div className=''>
-      <div className="navbar bg-base-100 p-0 min-h-[50px] border-b border-gray-300">
+    <div className='border-b border-gray-700'>
+      <div className="navbar bg-base-100 p-0 min-h-[50px] w-full md:w-11/12 mx-auto">
         <div className="navbar-start">
           <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <label tabIndex={0} className="btn btn-ghost btn-circle lg:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
             <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-              <li><NavLink className='h-[35px] rounded-full mx-1'><AiFillHome />Home</NavLink></li>
-              <li><NavLink className='h-[35px] rounded-full mx-1'><AiFillPicture />Media</NavLink></li>
-              <li><NavLink className='h-[35px] rounded-full mx-1'><AiFillMessage />Message</NavLink></li>
+              <li><NavLink to='/' className='h-[35px] rounded-full mx-1'><AiFillHome />Home</NavLink></li>
+              <li><NavLink to='/media' className='h-[35px] rounded-full mx-1'><AiFillPicture />Media</NavLink></li>
+              <li><NavLink to='/message' className='h-[35px] rounded-full mx-1'><AiFillMessage />Message</NavLink></li>
             </ul>
           </div>
           <Link className="btn btn-ghost normal-case text-xl">Bitit</Link>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal p-0">
-            <div className="tooltip tooltip-bottom h-[35px] my-auto" data-tip="Home"><li><NavLink to='/' className={({ isActive }) => isActive ? 'active rounded-full mx-3 px-10 h-[35px] my-auto active:bg-[#0066ff] text-xl' : 'undefined rounded-full mx-3 px-10 h-[35px] my-auto active:bg-[#0066ff] text-xl'}><AiFillHome /></NavLink></li></div>
-            <div className="tooltip tooltip-bottom h-[35px] my-auto" data-tip="Media"><li><NavLink to='/media' className='undefined rounded-full mx-5 px-10 h-[35px] my-auto active:bg-[#0066ff] text-xl'><AiFillPicture /></NavLink></li></div>
-            <div className="tooltip tooltip-bottom h-[35px] my-auto" data-tip="Message"><li><NavLink to='/message' className='undefined rounded-full mx-5 px-10 h-[35px] my-auto active:bg-[#0066ff] text-xl'><AiFillMessage /></NavLink></li></div>
+            <div className="tooltip tooltip-bottom h-[40px] my-auto" data-tip="Home"><li><NavLink to='/' className={({ isActive }) => isActive ? 'active rounded-full mx-3 px-10 h-[40px] my-auto active:bg-[#0066ff] text-2xl' : 'undefined rounded-full mx-3 px-10 h-[40px] my-auto active:bg-[#0066ff] text-2xl'}><AiFillHome /></NavLink></li></div>
+            <div className="tooltip tooltip-bottom h-[40px] my-auto" data-tip="Media"><li><NavLink to='/media' className='undefined rounded-full mx-5 px-10 h-[40px] my-auto active:bg-[#0066ff] text-2xl'><AiFillPicture /></NavLink></li></div>
+            <div className="tooltip tooltip-bottom h-[40px] my-auto" data-tip="Message"><li><NavLink to='/message' className='undefined rounded-full mx-5 px-10 h-[40px] my-auto active:bg-[#0066ff] text-2xl'><AiFillMessage /></NavLink></li></div>
           </ul>
         </div>
         <div className="navbar-end">
@@ -36,7 +36,7 @@ const Header = () => {
               </div>
             </label>
             <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-              <li><NavLink>About</NavLink></li>
+              <li><NavLink to=''>About</NavLink></li>
               <li><a>Logout</a></li>
             </ul>
           </div>
