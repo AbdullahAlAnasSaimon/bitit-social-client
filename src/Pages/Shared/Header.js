6a9b47eb138c4dfb71916c7c406aacb13dcd41e2
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-12-27 21:36:02 +0600

    added icons to header

--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import {AiFillHome, AiFillMessage, AiFillPicture} from 'react-icons/ai';
+import {HiUser} from 'react-icons/hi';
+
 
 const Header = () => {
   return (
@@ -11,36 +14,19 @@ const Header = () => {
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
             <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-              <li><Link>Item 1</Link></li>
-              <li tabIndex={0}>
-                <Link className="justify-between">
-                  Parent
-                  <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
-                </Link>
-                <ul className="p-2">
-                  <li><Link>Submenu 1</Link></li>
-                  <li><Link>Submenu 2</Link></li>
-                </ul>
-              </li>
-              <li><Link>Item 3</Link></li>
+              <li><Link>Home</Link></li>
+              <li tabIndex={0}><Link>Media</Link></li>
+              <li><Link>Message</Link></li>
             </ul>
           </div>
           <Link className="btn btn-ghost normal-case text-xl">Bitit</Link>
         </div>
         <div className="navbar-center hidden lg:flex">
-          <ul className="menu menu-horizontal px-1">
-            <li><Link>Item 1</Link></li>
-            <li tabIndex={0}>
-              <Link>
-                Parent
-                <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
-              </Link>
-              <ul className="p-2">
-                <li><Link>Submenu 1</Link></li>
-                <li><Link>Submenu 2</Link></li>
-              </ul>
-            </li>
-            <li><Link>Item 3</Link></li>
+          <ul className="menu menu-horizontal px-1 rounded-none">
+            <li><Link className='h-[35px] rounded-full mx-1'><AiFillHome/>Home</Link></li>
+            <li><Link className='h-[35px] rounded-full mx-1'><AiFillPicture/>Media</Link></li>
+            <li><Link className='h-[35px] rounded-full mx-1'><AiFillMessage/>Message</Link></li>
+            <li><Link className='h-[35px] rounded-full mx-1'><HiUser/>About</Link></li>
           </ul>
         </div>
         <div className="navbar-end">
