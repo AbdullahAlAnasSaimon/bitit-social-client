378684a51ec71324e9843c7d7aa3d217f451e932
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-12-27 21:43:37 +0600

    added avatar and icon for mobile device

--- src/Pages/Shared/Header.js
+++ src/Pages/Shared/Header.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import {AiFillHome, AiFillMessage, AiFillPicture} from 'react-icons/ai';
-import {HiUser} from 'react-icons/hi';
+import { NavLink } from 'react-router-dom';
+import { AiFillHome, AiFillMessage, AiFillPicture } from 'react-icons/ai';
 
 
 const Header = () => {
@@ -14,23 +13,32 @@ const Header = () => {
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
             <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-              <li><Link>Home</Link></li>
-              <li tabIndex={0}><Link>Media</Link></li>
-              <li><Link>Message</Link></li>
+            <li><NavLink className='h-[35px] rounded-full mx-1'><AiFillHome />Home</NavLink></li>
+            <li><NavLink className='h-[35px] rounded-full mx-1'><AiFillPicture />Media</NavLink></li>
+            <li><NavLink className='h-[35px] rounded-full mx-1'><AiFillMessage />Message</NavLink></li>
             </ul>
           </div>
-          <Link className="btn btn-ghost normal-case text-xl">Bitit</Link>
+          <NavLink className="btn btn-ghost normal-case text-xl">Bitit</NavLink>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1 rounded-none">
-            <li><Link className='h-[35px] rounded-full mx-1'><AiFillHome/>Home</Link></li>
-            <li><Link className='h-[35px] rounded-full mx-1'><AiFillPicture/>Media</Link></li>
-            <li><Link className='h-[35px] rounded-full mx-1'><AiFillMessage/>Message</Link></li>
-            <li><Link className='h-[35px] rounded-full mx-1'><HiUser/>About</Link></li>
+            <li><NavLink className='h-[35px] rounded-full mx-1'><AiFillHome />Home</NavLink></li>
+            <li><NavLink className='h-[35px] rounded-full mx-1'><AiFillPicture />Media</NavLink></li>
+            <li><NavLink className='h-[35px] rounded-full mx-1'><AiFillMessage />Message</NavLink></li>
           </ul>
         </div>
         <div className="navbar-end">
-          <Link className="btn">Get started</Link>
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src="https://placeimg.com/80/80/people" />
+              </div>
+            </label>
+            <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+              <li><NavLink>About</NavLink></li>
+              <li><a>Logout</a></li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
