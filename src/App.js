5e6cc756aa18466fe3ae9711b6673360910ecdc2
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-12-27 20:44:32 +0600

    create router and shared route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './Routes/Router';
 
 function App() {
   return (
     <div className="App">
-      <h2>bitit project</h2>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
