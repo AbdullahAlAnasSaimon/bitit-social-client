f89b24e5b987daa20e37653c9a7df994862ddf2e
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-12-29 01:33:48 +0600

    loaded posts to show in the media route

--- src/Pages/Home/Post/Post.js
+++ src/Pages/Home/Post/Post.js
@@ -9,13 +9,13 @@ import { AuthContext } from "../../../Context/AuthProvider/AuthProvider";
 import './Post.module.css';
 
 const Post = () => {
-  const {user} = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
   const [file, setFile] = useState(null);
   const [showArea, setShowArea] = useState(false);
   const { register, formState: { errors }, handleSubmit } = useForm();
   const [postText, setPostText] = useState(null);
   const imgHostKey = process.env.REACT_APP_IMGBB_KEY;
-  
+
   const handleDragAndDropArea = (e) => {
     e.preventDefault();
     setShowArea(true);
@@ -25,9 +25,9 @@ const Post = () => {
     setShowArea(false);
   }
 
-  const handlePostSubmit = (data, event) =>{
+  const handlePostSubmit = (data, event) => {
     const time = new Date().toString();
-    if(file === null){
+    if (file === null) {
       const postData = {
         user_name: user?.displayName,
         user_email: user?.email,
@@ -35,9 +35,23 @@ const Post = () => {
         post_text: data?.post,
         post_time: time,
       };
-      
+      console.log(postData);
+      fetch('http://localhost:5000/posts', {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(postData)
+      })
+        .then(res => res.json())
+        .then(data => {
+          if(data.acknowledged){
+            toast.success('Status Posted Successfully');
+          }
+        })
+        event.target.reset();
     }
-    else{
+    else {
       let image = file;
       const formData = new FormData();
       formData.append('image', image);
@@ -45,22 +59,34 @@ const Post = () => {
         method: 'POST',
         body: formData
       })
-      .then(res => res.json())
-      .then(imageData => {
-        if(imageData.success){
-          const postData = {
-            user_name: user?.displayName,
-            user_email: user?.email,
-            user_photo: user?.photoURL,
-            post_text: data?.post,
-            post_photo: imageData.data.url ? imageData.data.url : null,
-            post_time: time,
-          };
-          console.log(postData);
-        }
-      })
+        .then(res => res.json())
+        .then(imageData => {
+          if (imageData.success) {
+            const postData = {
+              user_name: user?.displayName,
+              user_email: user?.email,
+              user_photo: user?.photoURL,
+              post_text: data?.post,
+              post_photo: imageData.data.url ? imageData.data.url : null,
+              post_time: time,
+            };
+            console.log(postData);
+            fetch('http://localhost:5000/posts', {
+              method: 'POST',
+              headers: {
+                'content-type': 'application/json'
+              },
+              body: JSON.stringify(postData)
+            })
+              .then(res => res.json())
+              .then(data => {
+                if(data.acknowledged){
+                  toast.success('Status & Image Posted Successfully');
+                }
+              })
+          }
+        })
       event.target.reset();
-      image = null;
     }
   }
 
@@ -70,14 +96,14 @@ const Post = () => {
         user ? <>
           <form onSubmit={handleSubmit(handlePostSubmit)}>
             <div className='flex'>
-              <Link><span className='avatar w-12 rounded-full mr-2'>{user?.photoURL ? <img src={user?.photoURL} alt="" className='rounded-full w-10' /> : <HiUserCircle className="text-[41px]"/>}</span></Link>
-              <input {...register("post", {minLength: {value: 10, message: 'Write Minimum 10 Character in Input field'}, required: 'Write Minimum 10 Character in Input field'})} name="post" type="text" placeholder="What's on your mind? " className="input input-bordered border-2 border-transparent focus:outline-none focus:border-2 focus:border-blue-500 bg-zinc-900 w-full rounded-full" />
+              <Link><span className='avatar w-12 rounded-full mr-2'>{user?.photoURL ? <img src={user?.photoURL} alt="" className='rounded-full w-10' /> : <HiUserCircle className="text-[41px]" />}</span></Link>
+              <input {...register("post", { minLength: { value: 10, message: 'Write Minimum 10 Character in Input field' }, required: 'Write Minimum 10 Character in Input field' })} name="post" type="text" placeholder="What's on your mind? " className="input input-bordered border-2 border-transparent focus:outline-none focus:border-2 focus:border-blue-500 bg-zinc-900 w-full rounded-full" />
               <div className="tooltip tooltip-bottom z-10" data-tip="Upload Image"><button onClick={handleDragAndDropArea} className='btn rounded-full ml-2'><BsImages className='text-red-500' /></button></div>
             </div>
-              {errors.post && <p className="text-red-500 text-center"><small>*{errors?.post?.message}</small></p>}
+            {errors.post && <p className="text-red-500 text-center"><small>*{errors?.post?.message}</small></p>}
             <div className={`${showArea ? 'visible' : 'hidden'} mt-12 relative`} >
               <button onClick={handleCloseArea} className="btn btn-sm btn-circle absolute right-0 -top-10">✕</button>
-              <DragDrop file={file} setFile={setFile}/>
+              <DragDrop file={file} setFile={setFile} />
             </div>
             <button type="submit" className='bg-blue-500 hover:bg-blue-600 text-white mt-5 w-full font-semibold h-[40px] rounded-lg duration-300'>Post</button>
           </form>
@@ -89,8 +115,8 @@ const Post = () => {
 
 const fileTypes = ["JPG", "PNG", "GIF"];
 
-function DragDrop({file, setFile}) {
-  
+function DragDrop({ file, setFile }) {
+
   const handleChange = (file) => {
     setFile(file);
   };
--- src/Pages/Media/Media.js
+++ src/Pages/Media/Media.js
@@ -1,9 +1,34 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import Loading from '../Shared/Loading/Loading';
+import SinglePost from './SinglePost/SinglePost';
 
 const Media = () => {
+  const {data: posts, isLoading} = useQuery({
+    queryKey: ['userPost'],
+    queryFn: async() =>{
+      const res = await fetch('http://localhost:5000/posts');
+      const data = await res.json();
+      return data;
+    }
+  })
+  
+  if(isLoading){
+    return <Loading/>
+  }
+
   return (
-    <div>
-      
+    <div className='grid grid-cols-3'>
+      <div></div>
+      <div>
+        {
+          posts.map(post => <SinglePost
+          key={post?._id}
+          post={post}
+          ></SinglePost>)
+        }
+      </div>
+      <div></div>
     </div>
   );
 };
