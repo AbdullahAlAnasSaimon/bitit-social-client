e4cdd85bf9035c8e5a17b4de6ec5c61038d7e86f
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2022-12-28 14:02:24 +0600

    added input and form control to sign up page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,27 +1,34 @@
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
 
-  const { register, handleSubmit } = useForm();
-  const [data, setData] = useState("");
+  const { register, formState: {errors}, handleSubmit } = useForm();
+
+  const handleLogin = (data) =>{
+    console.log(data);
+  }
 
   return (
-    <div className='max-w-xs mx-auto my-10'>
-      <form onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}>
-        <h2 className='text-3xl font-bold'>Log In</h2>
-        <div className="form-control w-full max-w-xs my-5">
+    <div className='max-w-sm mx-auto my-10'>
+      <form onSubmit={handleSubmit(handleLogin)}>
+        <h2 className='text-3xl font-bold mb-8'>Log In</h2>
+        <div className="form-control w-full max-w-sm my-5">
           <p className='mb-2 text-sm'>Your Email</p>
-          <input {...register('email', {required: {message: 'Email is required'}})} type="text" name='email' placeholder="Email" className="input input-bordered w-full max-w-xs rounded-lg" />
+          <input {...register('email', {required: 'Email is required'})} type="text" name='email' placeholder="Email" className="input input-bordered w-full max-w-sm rounded-lg" />
+          {errors.email && <p className="text-red-500"><small>*{errors?.email?.message}</small></p>}
         </div>
-        <div className="form-control w-full max-w-xl my-5">
+        <div className="form-control w-full max-w-sm my-5">
           <p className='mb-2 text-sm'>Your Password</p>
-          <input {...register('password', {required: {message: 'Password is required'}})} type="password" name='password' placeholder="Password" className="input input-bordered w-full max-w-xs rounded-lg" />
+          <input {...register('password', {required: 'Password is required'})} type="password" name='password' placeholder="Password" className="input input-bordered w-full max-w-sm rounded-lg" />
+          {errors.password && <p className="text-red-500"><small>*{errors?.password?.message}</small></p>}
         </div>
         <div>
         <p className='text-sm'>Forget Password</p>
         </div>
         <button className='bg-blue-500 hover:bg-blue-600 text-white mt-5 w-full font-semibold h-[45px] rounded-lg duration-300' type="submit">Log In</button>
+        <p className='text-center mt-5'>New to Bitit? <Link to='/signup' className='text-blue-500 underline'>Create an account</Link></p>
       </form>
     </div>
   );
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
+
+  const { register, formState: {errors}, handleSubmit } = useForm();
+
+  const handleLogin = (data) =>{
+    console.log(data);
+  }
+
   return (
-    <div>
-      
+    <div className='max-w-sm mx-auto my-10'>
+      <form onSubmit={handleSubmit(handleLogin)}>
+        <h2 className='text-3xl font-bold mb-8'>Sign Up</h2>
+
+        <div className="form-control w-full max-w-sm my-5">
+          <p className='mb-2 text-sm'>Your Name</p>
+          <input {...register('name', {required: 'Name is required'})} type="text" name='name' placeholder="Name" className="input input-bordered w-full max-w-sm rounded-lg" required/>
+          {errors.name && <p className="text-red-500"><small>*{errors?.name?.message}</small></p>}
+        </div>
+        
+        <div className="form-control w-full max-w-sm my-5">
+          <p className='mb-2 text-sm'>Your Email</p>
+          <input {...register('email', {required: 'Email is required'})} type="text" name='email' placeholder="Email" className="input input-bordered w-full max-w-sm rounded-lg" required/>
+          {errors.email && <p className="text-red-500"><small>*{errors?.email?.message}</small></p>}
+        </div>
+
+        <div className="form-control w-full max-w-sm my-5">
+          <p className='mb-2 text-sm'>Your Password</p>
+          <input {...register('password', {required: 'Password is required'})} type="password" name='password' placeholder="Password" className="input input-bordered w-full max-w-sm rounded-lg" required/>
+          {errors.password && <p className="text-red-500"><small>*{errors?.password?.message}</small></p>}
+        </div>
+
+        <div className="form-control w-full max-w-sm my-5">
+          <p className='mb-2 text-sm'>Your Photo</p>
+          <input {...register('photo', {required: 'Photo is required'})} type="text" name='photo' placeholder="Photo URL" className="input input-bordered w-full max-w-sm rounded-lg" required/>
+          {errors.photo && <p className="text-red-500"><small>*{errors?.photo?.message}</small></p>}
+        </div>
+
+        <button className='bg-blue-500 hover:bg-blue-600 text-white mt-5 w-full font-semibold h-[45px] rounded-lg duration-300' type="submit">Sign Up</button>
+        <p className='text-center mt-5'>Already have an account? <Link to='/login' className='text-blue-500 underline'>Log In</Link></p>
+      </form>
     </div>
   );
 };
