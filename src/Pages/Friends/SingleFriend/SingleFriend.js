0a30fc5a85489bc3df4d4046a1c03577fd9707c5
AbdullahAlAnasSaimon <abdullahalanassaimon.swd@gmail.com>
2023-01-16 23:37:10 +0600

    changed button background color

--- src/Pages/Friends/SingleFriend/SingleFriend.js
+++ src/Pages/Friends/SingleFriend/SingleFriend.js
@@ -8,9 +8,9 @@ const SingleFriend = ({ friend }) => {
       <div className='flex items-center'>
         {photo ? <img className='w-16 h-16 rounded-full mr-3' src={photo} alt="user" /> : <HiUserCircle />}
         <div className='flex flex-col'>
-          <p className='mb-2 text-lg font-semibold'>{name}</p>
+          <p className='mb-2 text-lg'>{name}</p>
           <div>
-            <button className='bg-blue-600 hover:bg-blue-700 px-5 py-1 rounded-md duration-300 mr-2'>Add Friend</button>
+            <button className='bg-[#3551a4fd] hover:bg-[#2f4ab5] px-5 py-1 rounded-md duration-300 mr-2'>Add Friend</button>
             <button className='bg-zinc-600 hover:bg-zinc-700 px-5 py-1 rounded-md duration-300'>Remove</button>
           </div>
         </div>
